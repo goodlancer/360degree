116abea60045edf09dec5c50680404a77c5aea10
goodlancer <forethink888@gmail.com>
2020-11-15 18:47:28 +0300

    update server
    update server

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '.', 'build');
-const port = process.env.PORT || 4000;
-app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
    console.log('Server is up!');
 });
